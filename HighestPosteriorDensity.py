f858dc0b5c6450a69d054fdfb596dc2af441aedf
hellckt <hellckt@126.com>
2018-11-17 15:39:21 +0800

    fix

--- HighestPosteriorDensity.py
+++ HighestPosteriorDensity.py
@@ -1,3 +1,40 @@
 # -*- encoding=utf-8 -*-
 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import seaborn as sns
+from plot_post import plot_post
 
+
+def naive_hpd(post):
+    sns.kdeplot(post)
+    HPD = np.percentile(post, [2.5, 97.5])
+    plt.plot(HPD, [0, 0], label="HPD {:.2f} {:.2f}".format(*HPD), linewidth=4,
+             color='k')
+    plt.legend(fontsize=16)
+    plt.xlabel(r"$\theta$", fontsize=14)
+    plt.gca().axes.get_yaxis().set_ticks([])
+
+
+# Unimodal distribution
+# np.random.seed(1)
+# post = stats.beta.rvs(5, 11, size=1000)
+# naive_hpd(post)
+# plt.xlim(0, 1)
+# plt.show()
+
+# Bimodal distribution
+np.random.seed(1)
+gauss_a = stats.norm.rvs(loc=4, scale=0.9, size=3000)
+gauss_b = stats.norm.rvs(loc=-2, scale=1, size=2000)
+mix_norm = np.concatenate((gauss_a, gauss_b))
+
+
+# naive_hpd(mix_norm)
+# plt.savefig('./images/01_08.png', dpi=300, figsize=(5.5, 5.5))
+
+plot_post(mix_norm, roundto=2, alpha=0.05)
+plt.legend(loc=0, fontsize=16)
+plt.xlabel(r"$\theta$", fontsize=14)
+plt.show()
--- hpd.py
+++ hpd.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import scipy.stats.kde as kde
 
+
 def hpd_grid(sample, alpha=0.05, roundto=2):
     """Calculate highest posterior density (HPD) of array for given alpha. 
     The HPD is the minimum width Bayesian credible interval (BCI). 
@@ -30,30 +31,30 @@ def hpd_grid(sample, alpha=0.05, roundto=2):
     density = kde.gaussian_kde(sample)
     x = np.linspace(l, u, 2000)
     y = density.evaluate(x)
-    #y = density.evaluate(x, l, u) waitting for PR to be accepted
-    xy_zipped = zip(x, y/np.sum(y))
+    # y = density.evaluate(x, l, u) waitting for PR to be accepted
+    xy_zipped = zip(x, y / np.sum(y))
     xy = sorted(xy_zipped, key=lambda x: x[1], reverse=True)
     xy_cum_sum = 0
     hdv = []
     for val in xy:
         xy_cum_sum += val[1]
         hdv.append(val[0])
-        if xy_cum_sum >= (1-alpha):
+        if xy_cum_sum >= (1 - alpha):
             break
     hdv.sort()
-    diff = (u-l)/20  # differences of 5%
+    diff = (u - l) / 20  # differences of 5%
     hpd = []
     hpd.append(round(min(hdv), roundto))
     for i in range(1, len(hdv)):
-        if hdv[i]-hdv[i-1] >= diff:
-            hpd.append(round(hdv[i-1], roundto))
+        if hdv[i] - hdv[i - 1] >= diff:
+            hpd.append(round(hdv[i - 1], roundto))
             hpd.append(round(hdv[i], roundto))
     hpd.append(round(max(hdv), roundto))
     ite = iter(hpd)
     hpd = list(zip(ite, ite))
     modes = []
     for value in hpd:
-         x_hpd = x[(x > value[0]) & (x < value[1])]
-         y_hpd = y[(x > value[0]) & (x < value[1])]
-         modes.append(round(x_hpd[np.argmax(y_hpd)], roundto))
+        x_hpd = x[(x > value[0]) & (x < value[1])]
+        y_hpd = y[(x > value[0]) & (x < value[1])]
+        modes.append(round(x_hpd[np.argmax(y_hpd)], roundto))
     return hpd, x, y, modes
